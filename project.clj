145206598bf0e5f50b8808fa305bbd014db0a471
Juan-manuel Clavijo <clavijo.j@gmail.com>
2019-03-21 21:21:08 -0700

    Initial commit, cloned project, Clojure new project files added, and simple main function added to core file. Created empty test files.

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject pharmacy_counting "0.1.0-SNAPSHOT"
+  :description "Pharmacy Data Engineering Interview project"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.443"]
+                 [narkisr/cliopatra "1.1.0"]
+                 [clj-time "0.15.1"]]
+  :main pharmacy-counting.core
+  :jvm-opts ["-Xmx1g"]
+  :aliases {"launch" ["run" "./resources/input.txt"]}
+  :repositories [["java.net" "https://download.java.net/maven/2"]]
+  :profiles {:uberjar {:aot :all}}
+  :resource-paths ["input/"
+                   "output/"])
---
+++ src/pharmacy_counting/core.clj
@@ -0,0 +1,35 @@
+(ns pharmacy-counting.core
+  (:gen-class))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+;(def acct-map {})
+
+;;boot
+(defn -main
+  [& args]
+  (if-not (empty? args)
+    ;(try
+      (with-open [rdr (clojure.java.io/reader (last args))]
+        (binding [*in* rdr] 
+          (loop [line-input (do (read-line))
+                 acct-map []]
+            (if (nil? line-input)
+              (println "loop done");
+              (do
+                (println "-M:" line-input acct-map);;)
+                (recur (read-line)
+                       (conj acct-map line-input)
+                       )
+                ))
+            ))
+        )
+      ;; (catch Exception e (str "Error: File input type error or file not found. Check path and spelling.\n" 
+      ;;                         (last args) "\n"
+      ;;                         e)));try
+    (println "How to use this command: Enter the name of the text file input to process."
+             "'./input/input.txt'")))
+
